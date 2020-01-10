825043e03b097aa2e485d938e373cfe132adbd16
Naman Thanki <stuniz7855@gmail.com>
2020-01-10 06:34:47 +0530

    10 jan 20 morning commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -9,4 +9,14 @@ $ (document).ready(function(){
     }, {
         offset: '60px'
     });
+    $('.js--section-gamespage').waypoint(function(direction){
+        if(direction=="down"){
+            $('nav').addClass('sticky');
+        }
+        else{
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '-120px'
+    });
 });
